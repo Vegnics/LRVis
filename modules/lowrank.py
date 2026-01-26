7ca78079a5b92778514683e040056233f7f269e4
Amaranth <d12922028@csie.ntu.edu.tw>
2026-01-27 07:01:35 +0800

    ver 0.1

--- modules/lowrank.py
+++ modules/lowrank.py
@@ -283,7 +283,7 @@ class PreActBottleneckLR(nn.Module):
         super().__init__()
         
         self.lrgen = LRGenerator(4,4,N,in_planes,out_planes) if use_lr else None
-        self.convlr = nn.Conv2d(in_planes, out_planes, kernel_size=1, bias=True)
+        self.convlr = nn.Conv2d(out_planes, out_planes, kernel_size=1, bias=True)
 
         self.convlrmatch = nn.Conv2d(out_planes//2, out_planes//2, kernel_size=1, bias=True)
         
@@ -291,10 +291,10 @@ class PreActBottleneckLR(nn.Module):
         self.conv1 = nn.Conv2d(in_planes, in_planes//2, kernel_size=1, bias=True)
 
         self.bn2 = nn.BatchNorm2d(in_planes//2)
-        self.conv2 = nn.Conv2d(in_planes//2, in_planes//2, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(in_planes//2, in_planes//2, kernel_size=3, stride=stride, padding=1, bias=True)
 
         self.bn3 = nn.BatchNorm2d(in_planes//2)
-        self.conv3 = nn.Conv2d(in_planes//2, in_planes//2, kernel_size=1, bias=True)
+        self.conv3 = nn.Conv2d(in_planes//2, out_planes//2, kernel_size=1, bias=True)
         self.stride = stride
         self.shortcut = None
         if stride != 1 or in_planes != out_planes:
