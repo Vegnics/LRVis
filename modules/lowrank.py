bc9a3499f04d4fe639290ac00a79f173b4ec98ee
Amaranth <d12922028@csie.ntu.edu.tw>
2026-01-31 05:53:09 +0800

    testing full conv resnet

--- modules/lowrank.py
+++ modules/lowrank.py
@@ -285,13 +285,18 @@ class PreActBottleneck(nn.Module):
         super().__init__()
 
         self.bn1 = nn.BatchNorm2d(in_planes)
-        self.conv1 = nn.Conv2d(in_planes, in_planes//2, kernel_size=1, bias=False)
+        #self.conv1 = nn.Conv2d(in_planes, in_planes//2, kernel_size=1, bias=False)
+        self.conv1 = nn.Conv2d(in_planes, out_planes//2, kernel_size=1, bias=True)
+        
+        #self.bn2 = nn.BatchNorm2d(in_planes//2)
+        self.bn2 = nn.BatchNorm2d(out_planes//2)
+        #self.conv2 = nn.Conv2d(in_planes//2, in_planes//2, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(out_planes//2, out_planes//2, kernel_size=3, stride=stride, padding=1, bias=True)
 
-        self.bn2 = nn.BatchNorm2d(in_planes//2)
-        self.conv2 = nn.Conv2d(in_planes//2, in_planes//2, kernel_size=3, stride=stride, padding=1, bias=False)
-
-        self.bn3 = nn.BatchNorm2d(in_planes//2)
-        self.conv3 = nn.Conv2d(in_planes//2, out_planes, kernel_size=1, bias=False)
+        #self.bn3 = nn.BatchNorm2d(in_planes//2)
+        self.bn3 = nn.BatchNorm2d(out_planes//2)
+        #self.conv3 = nn.Conv2d(in_planes//2, out_planes, kernel_size=1, bias=False)
+        self.conv3 = nn.Conv2d(in_planes//2, out_planes, kernel_size=1, bias=True)
 
         self.shortcut = None
         if stride != 1 or in_planes != out_planes:
